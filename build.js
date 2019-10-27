9dc3c220ddb906c7d6aee5c597b321a5624e857e
Kip Reynolds <kip@kip-reynolds.net>
2019-10-27 11:05:48 -0700

    Additional changes

--- build.js
+++ build.js
@@ -1,14 +1,20 @@
 var Metalsmith = require('metalsmith');
 var layouts = require('metalsmith-layouts');
 var handlebars = require('jstransformer')(require('jstransformer-handlebars'));
+var browserSync = require('metalsmith-browser-sync');
+
+var templateConfig = {
+  engine: 'handlebars',
+  directory: 'layouts',
+  partials: 'partials',
+  default: 'main.hbs'
+}
  
 Metalsmith(__dirname)
   .source("src")
   .destination("build")
-  .use(layouts({
-    engine: "handlebars",
-    partials: "layouts/partials"
-  }))
+  .use(layouts(templateConfig))
+  .use(browserSync())
   .build(function(err) {
   if (err) console.log(err);
-});
+  });
