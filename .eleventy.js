c8b84e6f547e0defe3e967b8b8c82302e7018c45
Kip Reynolds <kip@kip-reynolds.net>
2019-11-02 21:41:18 -0700

    Updates to directory structure
    Creating partials and updating layouts to accommodate data.

--- .eleventy.js
+++ .eleventy.js
@@ -1,30 +1,16 @@
-let Nunjucks = require("nunjucks");
-
 module.exports = function(eleventyConfig) {
-  let nunjucksEnvironment = new Nunjucks.Environment(
-    new Nunjucks.FileSystemLoader("_includes")
-  );
 
-  eleventyConfig.setLibrary("njk", nunjucksEnvironment);
+  eleventyConfig.addPassthroughCopy("src/images");
 
   return {
-    templateFormats: ["md", "njk", "html"],
-
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about it.
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for URLs (it does not affect your file structure)
-    pathPrefix: "/",
-
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
     passthroughFileCopy: true,
+    templateFormats: ["md", "njk", "html"],
+    htmlTemplateEngine: "njk",
     dir: {
       input: "src",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
+      includes: "includes",
+      data: "data",
+      output: "site"
     }
   };
 };
